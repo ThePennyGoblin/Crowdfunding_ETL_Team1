f220260b18e59cba30ece6828094d0c33d5e08ce
aparsley1 <abigailparsley97@gmail.com>
2024-06-20 19:40:51 -0400

    crowdfunding db schema and select statements added

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,60 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Category" (
+    "category_id" varchar(10)   NOT NULL,
+    "category" varchar   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "Subcategory" (
+    "subcategory_id" varchar(10)   NOT NULL,
+    "subcategory" varchar   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "Contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar(30)   NOT NULL,
+    "last_name" varchar(30)   NOT NULL,
+    "email" varchar(60)   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "Campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" varchar   NOT NULL,
+    "discription" varchar   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" varchar   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar   NOT NULL,
+    "currency" varchar   NOT NULL,
+    "launch_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" varchar(10)   NOT NULL,
+    "subcategory_id" varchar(10)   NOT NULL,
+    CONSTRAINT "pk_Campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "Contacts" ("contact_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "Category" ("category_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Subcategory" ("subcategory_id");
+
+
---
+++ select_statements.sql
@@ -0,0 +1,11 @@
+--- selecting all items from Category table
+select * from "Category";
+
+--- selecting all items from Subcategory table
+select * from "Subcategory";
+
+--- selecting all items from Contacts table
+select * from "Contacts";
+
+--- selecting all items from Campaign table
+select * from "Campaign";
